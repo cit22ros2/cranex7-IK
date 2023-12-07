6850831d3321b2d711426fd695c7f948f267510c
Keitaro Nakamura <keitaro.nakamura@hoge.com>
2023-12-07 13:16:40 +0900

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -25,9 +25,9 @@ double IK(double L[], double xt[], double Rt[],double psi, double theta[]){
     double nor_xsw2;
     nor_xsw2 = x_sw[0]*x_sw[0] + x_sw[1]*x_sw[1] + x_sw[2]*x_sw[2];
     double r[3][3];
-    r[0][0] = cos(Rt[0])*cos(Rt[1])*cos(Rt[2]) + sin(Rt[0])*sin(Rt[2]);r[0][1] = -1*cos(Rt[0])*cos(Rt[1])*sin(Rt[2]) + -1*sin(Rt[0])*cos(Rt[2]);r[0][2] = cos(Rt[0])*sin(Rt[1]);
-    r[1][0] = sin(Rt[0])*cos(Rt[1])*cos(Rt[2]) + cos(Rt[0])*sin(Rt[2]);r[1][1] = -1*sin(Rt[0])*cos(Rt[1])*sin(Rt[2]) + cos(Rt[0])*cos(Rt[2]);r[1][2] = sin(Rt[0])*sin(Rt[1]);
-    r[2][0] = -1*sin(Rt[1])*cos(Rt[2]);r[2][1] = sin(Rt[1])*sin(Rt[2]);r[2][2] = cos(Rt[1]);
+    r[0][0] = cos(Rt[1])*cos(Rt[2]) ;  r[0][1] = -1*sin(Rt[1])*sin(Rt[2]) ;  r[0][2] = sin(Rt[1]);
+    r[1][0] = sin(Rt[0])*sin(Rt[1])*cos(Rt[2]) + cos(Rt[1])*sin(Rt[2]);  r[1][1] = -1*sin(Rt[0])*sin(Rt[1])*sin(Rt[2]) + cos(Rt[0])*cos(Rt[2]);  r[1][2] = -1*sin(Rt[0])*cos(Rt[1]);
+    r[2][0] = -1*cos(Rt[0])*sin(Rt[1])*cos(Rt[2]) + sin(Rt[0])*sin(Rt[2]);  r[2][1] = cos(Rt[0])*sin(Rt[1])*sin(Rt[2]) + sin(Rt[0])*cos(Rt[2]);  r[2][2] = cos(Rt[0])*cos(Rt[1]);
     double u_sw[3];
     u_sw[0] = x_sw[0]/sqrt(nor_xsw2);
     u_sw[1] = x_sw[1]/sqrt(nor_xsw2);
@@ -42,8 +42,6 @@ double IK(double L[], double xt[], double Rt[],double psi, double theta[]){
     S2 = -1*( M*sqrt( x_sw[0]*x_sw[0] + x_sw[1]*x_sw[1] ) + N*x_sw[2] )/( N*N + M*M );
     C2 = ( N*sqrt( x_sw[0]*x_sw[0] + x_sw[1]*x_sw[1] ) - M*x_sw[2] )/( N*N + M*M );
     theta20 = atan2( S2 , C2 );
-    std::cout << theta10 << std::endl;
-    std::cout << theta20 << std::endl;
 
     double As[3][3],Bs[3][3],Cs[3][3];
     As[0][0] = -1*u_sw[2]*r[1][0] + u_sw[1]*r[2][0];
@@ -70,9 +68,16 @@ double IK(double L[], double xt[], double Rt[],double psi, double theta[]){
     Cs[2][1] = u_sw[0]*u_sw[2]*r[0][1] + u_sw[1]*u_sw[2]*r[1][1] + r[2][1]*u_sw[2]*u_sw[2];
     Cs[2][2] = u_sw[0]*u_sw[2]*r[0][2] + u_sw[1]*u_sw[2]*r[1][2] + r[2][2]*u_sw[2]*u_sw[2];
 
-    theta[0] = atan2( -1*( As[1][1]*sin(psi) + Bs[1][1]*cos(psi) + Cs[1][1] ), -1*( As[0][1]*sin(psi) + Bs[0][1]*cos(psi) + Cs[0][1] ) );
-    theta[1] = acos( -1*( As[2][1]*sin(psi) + Bs[2][1]*cos(psi) + Cs[2][1] ) );
-    theta[2] = atan2( As[2][2]*sin(psi) + Bs[2][2]*cos(psi) + Cs[2][2] , -1*(As[2][0]*sin(psi) + Bs[2][0]*cos(psi) + Cs[2][0] ) );
+    double theta01,theta02,theta1,theta21,theta22;
+    theta01 = -1*( As[1][1]*sin(psi) + Bs[1][1]*cos(psi) + Cs[1][1] );
+    theta02 = -1*( As[0][1]*sin(psi) + Bs[0][1]*cos(psi) + Cs[0][1] );
+    theta1  = -1*( As[2][1]*sin(psi) + Bs[2][1]*cos(psi) + Cs[2][1] );
+    theta21 =      As[2][2]*sin(psi) + Bs[2][2]*cos(psi) + Cs[2][2];
+    theta22 = -1*( As[2][0]*sin(psi) + Bs[2][0]*cos(psi) + Cs[2][0] );
+    std::cout << "theta01 " << theta01 << " theta02 " << theta02 << " theta1 " << theta1 << " theta21 " << theta21 << " theta22 " << theta22 << std::endl;
+    theta[0] = atan2( theta01 , theta02);
+    theta[1] = acos( theta1 );
+    theta[2] = atan2( theta21 , theta22 );
 
     double Aw[3][3],Bw[3][3],Cw[3][3];
     Aw[0][2] = r[0][2]*( As[0][0]*cos(L[3]) + As[0][1]*sin(L[3]) ) + r[1][2]*( As[1][0]*cos(L[3]) + As[1][1]*sin(L[3]) ) + r[2][2]*( As[2][0]*cos(L[3]) + As[2][1]*sin(L[3]) );
@@ -103,10 +108,11 @@ double IK(double L[], double xt[], double Rt[],double psi, double theta[]){
 
 int main(void){
     double L[4] = { 0.105,0.250,0.250,0.060 };
-    double xt[3] = { 0.1,0.2,0.05 };
-    double Rt[3] = { 90,0,90 };
+    double xt[3] = { 0.353,0,0.105 };
+    double Rt[3] = { 0,0,90 };
     double theta[7] = {0};
-    double psi=0;
+    double psi;psi=30*M_PI/180;
+
     IK(L,xt,Rt,psi,theta);
     for(auto ar : theta){
         std::cout << ar << std::endl;
